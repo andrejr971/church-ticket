8debb9aedd548c4e26dd2956c046fdb1d400fa58
André Junior <andrejunior179@gmail.com>
2024-05-04 09:30:58 -0300

    feat: add count day

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,13 +1,33 @@
+'use client'
+
+import { useMemo } from 'react'
+
 import { gradientColors } from '@/lib/colors'
 
 import { ModalTicket } from './modal-ticket'
 
 export function Banner() {
+  const diffDays: string = useMemo(() => {
+    const dateStart = new Date()
+    const dateEnd = new Date('2024/05/26 08:00:00')
+
+    const diff = parseInt(
+      String((dateEnd.getTime() - dateStart.getTime()) / (1000 * 60 * 60 * 24)),
+      10,
+    )
+
+    if (diff < 0) {
+      return 'O evento já aconteceu'
+    }
+
+    return `FALTAM APENAS ${diff} ${diff === 1 ? 'DIA' : 'DIAS'}`
+  }, [])
+
   return (
     <header
       className={`flex items-center justify-center gap-4 ${gradientColors.blue} p-4 py-2`}
     >
-      <p className="font-medium uppercase text-white">FALTAM APENAS 30 DIAS</p>
+      <p className="font-medium uppercase text-white">{diffDays}</p>
 
       <ModalTicket
         configButtonTrigger={{
